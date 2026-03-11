317e8a04bf7d496ec9cc6876e12180e88b0560b5
GabrielDebug <gabrielmessiasc0d3@gmail.com>
2026-03-11 08:01:52 -0300

    Create class Produto

--- MeuCrud/Program.cs
+++ MeuCrud/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+namespace Crud
+{
+	class produto
+	{
+		public int Id { get; set; }
+		public string Nome { get; set; } = "";
+		public double Preco { get; set; }
+	} 
+	class ProdutoRepositorio
+	}
