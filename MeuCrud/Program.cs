4d5503543acb5cddb83e81afcf85a1e756f329d8
GabrielDebug <gabrielmessiasc0d3@gmail.com>
2026-03-11 08:05:31 -0300

    Create New List Product

--- MeuCrud/Program.cs
+++ MeuCrud/Program.cs
@@ -5,11 +5,21 @@ using System.Text.Json;
 
 namespace Crud
 {
-	class produto
+	class Produto
 	{
 		public int Id { get; set; }
 		public string Nome { get; set; } = "";
 		public double Preco { get; set; }
 	} 
 	class ProdutoRepositorio
+	{
+		private readonly string _arquivo = "produto.json";
+
+		private List<Produto> Ler()
+		{
+			if(!File.Exists(_arquivo)) return new List<Produto>();
+			var json = File.ReadAllText(_arquivo);
+			return JsonSerializer.Deserialize<List<Produto>>(json) ?? new List<Produto>();
+		}
+	}
 	}
