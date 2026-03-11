bf9f1806617d092edbbecafffa8a59dbcf446518
GabrielDebug <gabrielmessiasc0d3@gmail.com>
2026-03-11 08:23:48 -0300

    Adicionar função Remover por ID

--- MeuCrud/Program.cs
+++ MeuCrud/Program.cs
@@ -10,17 +10,17 @@ namespace Crud
 		public int Id { get; set; }
 		public string Nome { get; set; } = "";
 		public double Preco { get; set; }
-	} 
+	}
 	class ProdutoRepositorio
 	{
 		private readonly string _arquivo = "produto.json";
 
 		private List<Produto> Ler()
 		{
-			if(!File.Exists(_arquivo)) return new List<Produto>();
+			if (!File.Exists(_arquivo)) return new List<Produto>();
 			var json = File.ReadAllText(_arquivo);
 			return JsonSerializer.Deserialize<List<Produto>>(json) ?? new List<Produto>();
-		}	
+		}
 
 		private void Salvar(List<Produto> produtos)
 		{
@@ -41,22 +41,53 @@ namespace Crud
 		public void ListarTodos()
 		{
 			var lista = Ler();
-			if(lista.Count == 0) {Console.WriteLine("Nenhum produto cadastrado"); return;}
+			if (lista.Count == 0) { Console.WriteLine("Nenhum produto cadastrado"); return; }
 			Console.WriteLine("\n Produtos Cadastrados:");
-			Console.WriteLine($"{"ID", -5} {"Nome", -20} {"Preco", 10}");
-		  foreach(var p in lista)
+			Console.WriteLine($"{"ID",-5} {"Nome",-20} {"Preco",10}");
+			foreach (var p in lista)
 			{
-				Console.WriteLine($"{p.Id, -5} {p.Nome, -20} {p.Preco, 10:C}");
+				Console.WriteLine($"{p.Id,-5} {p.Nome,-20} {p.Preco,10:C}");
 			}
 		}
 		public Produto? BuscarPorId(int id)
 		{
 			var produto = Ler().Find(p => p.Id == id);
-			if(produto == null)
+			if (produto == null)
 			{
 				Console.WriteLine($"Produto com ID {id} não encontrado.");
 				return null;
 			}
+			return produto;
+		}
+		public void Atualizar(int id, string novoNome, double novoPreco)
+		{
+			var lista = Ler();
+			var produto = lista.Find(p => p.Id == id);
+			if (produto == null)
+			{
+				Console.WriteLine($"Produto com ID {id} não encontrado.");
+				return;
+			}
+
+			produto.Nome = novoNome;
+			produto.Preco = novoPreco;
+			Salvar(lista);
+
+			Console.WriteLine($"Produto com ID {id} atualizado com sucesso!");
+		}
+		public void Remover(int id)
+		{
+			var lista = Ler();
+			var produto = lista.Find(p => p.Id == id);
+			if (produto == null)
+			{
+				Console.WriteLine($"Produto com ID {id} não encontrado.");
+				return;
+			}
+			lista.Remove(produto);
+			Salvar(lista);
+
+			Console.WriteLine($"Produto com ID {id} removido com sucesso!");
 		}
 	}
-	}
+}
